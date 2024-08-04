e57d7cf4c882fc968f0e3a361f47689b9a0a67e6
Julián Martinez <julian.itsa8@gmail.com>
2024-08-04 22:41:54 +0200

    Created Xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Julian Martinez Cordones on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
