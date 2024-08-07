d9f6b03171f523f93703a43075f15f359f41e64e
Julián Martinez <julian.itsa8@gmail.com>
2024-08-07 17:08:58 +0200

    Average step count calculation

---
+++ Step Tracker/Extensions/Date+Ext.swift
@@ -0,0 +1,14 @@
+//
+//  Date+Ext.swift
+//  Step Tracker
+//
+//  Created by Julian Martinez Cordones on 7/8/24.
+//
+
+import Foundation
+
+extension Date {
+    var weekdayInt: Int {
+        Calendar.current.component(.weekday, from: self)
+    }
+}
